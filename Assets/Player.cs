e2e333ce2f2711f0f520f401c5bd8520b61fe812
MehdiZarei <alexmettkov@gmail.com>
2023-09-12 12:38:28 +0330

    initial commit!

---
+++ Assets/Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private void Start() {
+        Debug.Log("initial call");
+    }
+}
